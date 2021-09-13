84f6f8dbe76d4930f6c42eb95770b20bf60553ec
theeyx <62816613+theeyx@users.noreply.github.com>
2021-09-13 14:29:42 -0700

    Add files via upload

---
+++ C++/kidsWithTheGreatestNumberOfCandies1431.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int maxCandy = *max_element(candies.begin(), candies.end());
+        vector<bool> result(candies.size());
+        for(int i = 0; i < candies.size(); ++i){
+            result[i] = (candies[i] + extraCandies >= maxCandy);
+        }
+        return result;
+    }
+};
